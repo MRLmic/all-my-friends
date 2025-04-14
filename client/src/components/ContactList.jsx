05d63f552c82150296a7912b2e14ab5b91635b10
Melissa Luna <missi.luna@gmail.com>
2025-04-13 20:24:29 -0400

    Create ContactList component

---
+++ client/src/components/ContactList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactList = () => {
+    return (
+        <div className='contact-list'>
+            <h1>Contact List</h1>
+        </div>
+    );
+};
+
+export default ContactList;
