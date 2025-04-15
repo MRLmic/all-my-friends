da8e0b4483128ec1b9ebb5a160c2fa46a0c49e12
Melissa Luna <missi.luna@gmail.com>
2025-04-15 14:49:38 -0400

    Insert Offcanvas component

--- client/src/components/ContactList.jsx
+++ client/src/components/ContactList.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Offcanvas } from "react-bootstrap";
+
+import ListEntry from "./ListEntry";
 
 const ContactList = () => {
-    return (
-        <div className='contact-list'>
-            <h1>Contact List</h1>
-        </div>
-    );
+  return (
+    <div className="contact-list">
+      <Offcanvas show={true} placement={"start"}>
+        <Offcanvas.Header>
+          <Offcanvas.Title>Contact List</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+            <ListEntry />
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
 };
 
 export default ContactList;
