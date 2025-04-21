83a20a14acd78e3514d9a25fd11333cec677a103
Melissa Luna <missi.luna@gmail.com>
2025-04-21 02:08:08 -0400

    Create Contact DTO

---
+++ server/Data/ContactDto.cs
@@ -0,0 +1,11 @@
+namespace Server.Data
+{
+    public class ContactDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; } 
+        public string Label { get; set; }
+        public string Phone { get; set; }
+    }
+}
