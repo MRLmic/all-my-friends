7fa01dd36db1a3860bcba1a2ae738352bbff3cb6
Melissa Luna <missi.luna@gmail.com>
2025-04-20 23:45:35 -0400

    Add parameterless constructor

--- server/Models/Contact.cs
+++ server/Models/Contact.cs
@@ -8,12 +8,14 @@ namespace Server.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public ICollection<ContactDetail> ContactDetails { get; set; } = new List<ContactDetail>();
+        public ICollection<ContactDetail> ContactDetails { get; private set; } 
 
         public Contact(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
         }
+
+        private Contact() {}
     }
 }
