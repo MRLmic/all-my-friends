862f4466671e9f5964b1c49161bab2501839b7da
Melissa Luna <missi.luna@gmail.com>
2025-05-01 20:39:41 -0400

    Add addContactDetail button

--- client/src/components/ContactDetails.jsx
+++ client/src/components/ContactDetails.jsx
@@ -3,17 +3,25 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { MdModeEdit } from "react-icons/md";
 import { LuTrash } from "react-icons/lu";
+import { TbPhonePlus } from "react-icons/tb";
 
 import ContactDetailsItem from "./ContactDetailsItem";
 
-const ContactDetails = ({ selectedContact, setEditContactForm, setShowForm }) => {
+const ContactDetails = ({
+  selectedContact,
+  setEditContactForm,
+  setShowForm,
+}) => {
   const { firstName, lastName, contactDetails } = selectedContact;
-
 
   const handleEditClick = () => {
     setEditContactForm(true);
     setShowForm(true);
-  }
+  };
+
+  const handleAddDetailsClick = () => {
+    console.log("Add details clicked");
+  };
 
   return (
     <div className="contact-details">
@@ -22,7 +30,18 @@ const ContactDetails = ({ selectedContact, setEditContactForm, setShowForm }) =>
         {firstName} {lastName}
       </div>
       <div className="d-inline-block mb-2">
-        <Button className="mx-1" variant="outline-info" onClick={() => handleEditClick()}>
+        <Button
+          className="mx-1"
+          variant="outline-success"
+          onClick={() => handleAddDetailsClick()}
+        >
+          <TbPhonePlus />
+        </Button>
+        <Button
+          className="mx-1"
+          variant="outline-info"
+          onClick={() => handleEditClick()}
+        >
           <MdModeEdit className="edit-icon" />
         </Button>
         <Button className="mx-1" variant="outline-danger">
@@ -30,14 +49,14 @@ const ContactDetails = ({ selectedContact, setEditContactForm, setShowForm }) =>
         </Button>
       </div>
       <div>
-      {contactDetails.map((contactDetail, index) => (
-        <ContactDetailsItem
-          key={index}
-          label={contactDetail.label}
-          region={contactDetail.region}
-          phoneNumber={contactDetail.phoneNumber}
-        />
-      ))}
+        {contactDetails.map((contactDetail, index) => (
+          <ContactDetailsItem
+            key={index}
+            label={contactDetail.label}
+            region={contactDetail.region}
+            phoneNumber={contactDetail.phoneNumber}
+          />
+        ))}
       </div>
     </div>
   );
