5d297b949655bb88c5543fd708c28b42789a198c
Melissa Luna <missi.luna@gmail.com>
2025-05-07 04:56:37 -0400

    required check

--- server/Models/Contact.cs
+++ server/Models/Contact.cs
@@ -1,13 +1,15 @@
 using PhoneNumbers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Server.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        [Required]
         public ICollection<ContactDetail> ContactDetails { get; set; } 
 
         public Contact(string firstName, string lastName)
--- server/Models/ContactDetail.cs
+++ server/Models/ContactDetail.cs
@@ -1,16 +1,19 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Server.Models
 {
     public class ContactDetail
     {
         public int Id { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
         public string Region { get; set; }
+        [Required]
         public string Label { get; set; }
         public int ContactId { get; set; }
 
-        [JsonIgnore] //TODO - Remove after DTO is mapped
+        [JsonIgnore] 
         public Contact? Contact { get; set; }
     }
 }
